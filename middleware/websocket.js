f7d127779f8feda566f69b643f472ad0713ade70
MinallW <suarezmiguelc23@gmail.com>
2021-04-22 10:15:23 -0500

    Added websockets API and React Client Connection

--- middleware/websocket.js
+++ middleware/websocket.js
@@ -1,15 +1,18 @@
-const express = require('express'),
-      app = express(),
-      http = require('http'),
-      server = http.createServer(app),
-      io = require('socket.io')(server)
+const app = require('express')(),
+      httpServer = require('http').createServer(app)
+
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: '*'
+  }
+})
 
 const kafka = require('kafka-node'),
       Consumer = kafka.Consumer,
       consumerClient = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' }),
       consumer = new Consumer(
         consumerClient,
-        [{ topic: 'kafka-chat0' }]
+        [{ topic: 'kafka-chat7' }]
       )
 
 const Producer = kafka.Producer,
@@ -17,12 +20,14 @@ const Producer = kafka.Producer,
       producer = new Producer(producerClient)
 
 producer.on('ready', function() {
-  produceMessage = (msg) => {
-    producer.send([{topic: 'kafka-chat0', messages: [msg]}], function(err, data) { 
-     console.log(data)
-    })
-  }  
+  console.log('producer is ready')
 })
+
+const produceMessage = (msg) => {
+  producer.send([{topic: 'kafka-chat7', messages: [msg]}], function(err, data) { 
+    console.log(data)
+  })
+}  
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
@@ -30,16 +35,16 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
 
-  console.log('User Connected')
-  consumer.on('message', function(message) {
-    socket.emit('chat message', message.value)
+  socket.broadcast.emit('user connection', socket.id)
+  
+  consumer.on('message', (msg) => {
+    socket.emit('chat message', msg.value)
   })
-
   socket.on('produce', (msg) => {
     produceMessage(msg)
   })
 })
 
-server.listen(3001, () => {
-  console.log('listening on *:3001')
+httpServer.listen(8080, () => {
+  console.log('listening on *:8080')
 })
